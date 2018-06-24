16b499ce1887ed38747d3428080440b10ff85241
Blaine Richardson <blainerichardson@Blaines-MacBook-Pro.local>
2018-06-24 14:12:46 -0500

    (init) adding first version of library

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,6 @@ const minify = require('gulp-minify');
 const jsonminify = require('gulp-jsonminify');
 const del = require('del'); //eslint-disable-line
 
-gulp.task('copy:md', (done) => {
-  gulp.src('*.md')
-    .pipe(gulp.dest('build/dist'))
-    .on('end', () => {
-      done();
-    })
-    .on('error', (err) => {
-      done(err);
-    });
-});
-
-gulp.task('copy:lsc', (done) => {
-  gulp.src('LICENSE')
-    .pipe(gulp.dest('build/dist'))
-    .on('end', () => {
-      done();
-    })
-    .on('error', (err) => {
-      done(err);
-    });
-});
-
 gulp.task('compress:js', (done) => {
   gulp.src('src/*.js')
     .pipe(minify({
@@ -44,31 +22,8 @@ gulp.task('compress:js', (done) => {
     });
 });
 
-gulp.task('compress:json', (done) => {
-  gulp.src(['*.json'])
-    .pipe(jsonminify())
-    .pipe(gulp.dest('build/tmp'))
-    .on('end', () => {
-      done();
-    })
-    .on('error', (err) => {
-      done(err);
-    });
-});
-
 gulp.task('transfer:js', (done) => {
   gulp.src('build/tmp/*.js')
-    .pipe(gulp.dest('build/dist'))
-    .on('end', () => {
-      done();
-    })
-    .on('error', (err) => {
-      done(err);
-    });
-});
-
-gulp.task('transfer:json', (done) => {
-  gulp.src('build/tmp/*.json')
     .pipe(gulp.dest('build/dist'))
     .on('end', () => {
       done();
@@ -94,12 +49,8 @@ gulp.task('clean:post', (done) => {
 });
 
 gulp.task('default', gulp.series(
-  'copy:md',
-  'copy:lsc',
   'compress:js',
-  'compress:json',
   'transfer:js',
-  'transfer:json',
   'transfer:build',
   'clean:post'
 ));
